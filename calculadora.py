ef5e307de73d32b76843328ec0d77eb0c71c0a33
joseph <josephdonald.pi@gmail.com>
2026-09-11 19:11:11 -0300

    adicionei a função de subtração

--- calculadora.py
+++ calculadora.py
@@ -5,3 +5,9 @@ def adicao(num1, num2):
     return soma
 
 print("Resultado da soma: ", adicao(16, 9))
+
+def subtracao(num1, num2):
+    resultado = num1 - num2
+    return resultado
+
+print("Resultado da subtração:", subtracao(89,33))
